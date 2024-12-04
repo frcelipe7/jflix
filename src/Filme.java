ee1826b9ead5a94e1d35c7ee3270ec0d10515bc8
Felipe Raphael Coelho dos Santos <feliperaphaelpara@gmail.com>
2024-12-04 16:02:10 -0300

    adding get & set

--- src/Filme.java
+++ src/Filme.java
@@ -1,10 +1,95 @@
 public class Filme {
-    String nomeDoFilme;
-    int anoLancamento = 0;
-    boolean incluidoNoPlano;
-    double notaDoFilme;
-    int avaliacao;
-    int totalDeAvaliacoes;
-    String sinopse;
-    int duracaoEmMinutos;
+    ////////////////////////////////////////////////
+    // ATRIBUTOS
+    ///////////////////////////////////////////////
+    private String nomeDoFilme;
+    private int anoLancamento = 0;
+    private boolean incluidoNoPlano;
+    private double notaDoFilme;
+    private int avaliacao;
+    private int totalDeAvaliacoes;
+    private String sinopse;
+    private int duracaoEmMinutos;
+
+    ////////////////////////////////////////////////
+    // METODOS
+    ///////////////////////////////////////////////
+
+    // construtor
+    public Filme(String nomeDoFilme, int anoLancamento, boolean incluidoNoPlano, double notaDoFilme, int avaliacao, int totalDeAvaliacoes, String sinopse, int duracaoEmMinutos) {
+        this.nomeDoFilme = nomeDoFilme;
+        this.anoLancamento = anoLancamento;
+        this.incluidoNoPlano = incluidoNoPlano;
+        this.notaDoFilme = notaDoFilme;
+        this.avaliacao = avaliacao;
+        this.totalDeAvaliacoes = totalDeAvaliacoes;
+        this.sinopse = sinopse;
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    // serialize method
+    public String serialize() {
+        return "{\"nomeDoFilme\": \"" + this.nomeDoFilme + "\", \"anoLancamento\": " + this.anoLancamento + ", \"incluidoNoPlano\": " + this.incluidoNoPlano + ", \"notaDoFilme\": " + this.notaDoFilme + ", \"avaliacao\": " + this.avaliacao + ", \"totalDeAvaliacoes\": " + this.totalDeAvaliacoes + ", \"sinopse\": \"" + this.sinopse + "\", \"duracaoEmMunitos\": " + this.duracaoEmMinutos + "}";
+    }
+
+    // set & get
+    public void setNomeDoFilme(String nome) {
+        this.nomeDoFilme = nome;
+    }
+
+    public String getNomeDoFilme() {
+        return this.nomeDoFilme;
+    }
+
+    public void setAnoLancamento(int ano) {
+        this.anoLancamento = ano;
+    }
+
+    public int getAnoLancamento() {
+        return this.anoLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluido) {
+        this.incluidoNoPlano = incluido;
+    }
+
+    public void setNotaDoFilme(double nota) {
+        this.notaDoFilme = nota;
+    }
+
+    public double getNotaDoFilme() {
+        return this.notaDoFilme;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public int getAvaliacao() {
+        return this.avaliacao;
+    }
+
+    public void setTotalDeAvaliacoes(int total) {
+        this.totalDeAvaliacoes = total;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return this.totalDeAvaliacoes;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getSinopse() {
+        return this.sinopse;
+    }
+
+    public void setDuracaoEmMinutos(int duracao) {
+        this.duracaoEmMinutos = duracao;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return this.duracaoEmMinutos;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Filme filme = new Filme();
+        String nomeDoFilme = "Moana 2";
+        int anoLancamento = 2024;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 10.0;
+        int avaliacao = 10;
+        int totalDeAvaliacoes = 1;
+        String sinopse = "Esta é a sinopse do filme Moana 2";
+        int duracaoEmMinutos = 150;
+
+        Filme meuFilme = new Filme(nomeDoFilme, anoLancamento, incluidoNoPlano, notaDoFilme, avaliacao, totalDeAvaliacoes, sinopse, duracaoEmMinutos);
+
+        String filmeJson = meuFilme.serialize();
+
+        System.out.println(filmeJson);
+
+        String nome = meuFilme.getNomeDoFilme();
+
+        System.out.println(nome);
     }
 }
