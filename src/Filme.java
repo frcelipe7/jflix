3fed9266d43ecd4531fa13c6c95e2989e4f3c772
Felipe Raphael Coelho dos Santos <feliperaphaelpara@gmail.com>
2024-12-04 10:04:29 -0300

    inserindo Filme class

---
+++ src/Filme.java
@@ -0,0 +1,10 @@
+public class Filme {
+    String nomeDoFilme;
+    int anoLancamento = 0;
+    boolean incluidoNoPlano;
+    double notaDoFilme;
+    int avaliacao;
+    int totalDeAvaliacoes;
+    String sinopse;
+    int duracaoEmMinutos;
+}
